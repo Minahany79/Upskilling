867d70f65639d2ea38faa20112de2911e55603ae
mina79 <mina.hany460@gmail.com>
2023-10-19 20:59:16 +0300

    [add] allow cors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
 import contactRouter from "./routes/contact-us";
+import cors from "cors";
 import dotenv from "dotenv";
 
 dotenv.config(); // Load environment variables from .env
@@ -8,13 +9,13 @@ dotenv.config(); // Load environment variables from .env
 const app = express();
 const port = process.env.PORT;
 
+app.use(cors());
+app.use(bodyParser.json());
+
 app.get("/ping", (req: Request, res: Response) => {
   res.status(200).json({ message: `PONG !! from UpSkilling - ${new Date()}` });
 });
 
-app.use(bodyParser.json());
-
-// Use the email router for handling email requests
 app.use("/", contactRouter);
 
 app.listen(port, () => {
